01798fe4fabc6216dfc5dceb8b74a872c0e45832
Amit Patange <amitpatange88@gmail.com>
2018-11-04 22:47:38 +0530

    comment added

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace DesignPatterns
 {
+    /// <summary>
+    /// Design Patterns are covered here in this project.
+    /// </summary>
     class Program
     {
         static void Main(string[] args)

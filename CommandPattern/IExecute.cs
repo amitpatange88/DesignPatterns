3b6b6baee8a4058abb3c8943436f50873792e38e
Amit Patange <amitpatange88@gmail.com>
2018-12-08 10:20:11 +0530

    Flyweight design pattern

--- CommandPattern/IExecute.cs
+++ CommandPattern/IExecute.cs
@@ -10,6 +10,13 @@ namespace CommandPattern
     {
         public string strCommand = string.Empty;
 
+        public static string test = string.Empty;
+        
+        static IExecute()
+        {
+
+        }
+
         public virtual void CmdExecute()
         {
 
